ea2d7d1276807d682af91e6cb130df094e3b9216
DanCaldwell3D <73749451+DanCaldwell3D@users.noreply.github.com>
2024-08-13 18:25:27 +1000

    added getter/setter

--- tutorials/classes_realpython/circle.py
+++ tutorials/classes_realpython/circle.py
@@ -6,5 +6,15 @@ class Circle:
     def __init__(self, radius):
         self.radius = radius
 
+    @property
+    def radius(self):
+        return self._radius
+    
+    @radius.setter
+    def radius(self, value):
+        if not isinstance(value, int | float) or value <= 0:
+            raise ValueError("Positive number expected")
+        self._radius = value
+    
     def calculate_area(self):
         return (math.pi * self.radius ** 2)
